caf0a4ac9f3abb66858197a12c3fd12a298864e8
Martin Alejandro Ribelotta <martinribelotta@gmail.com>
2020-01-25 23:45:36 -0300

    Travis CI: Fix build package creation
    Signed-off-by: Martin Alejandro Ribelotta <martinribelotta@gmail.com>

--- ci/tests-ci.sh
+++ ci/tests-ci.sh
@@ -41,10 +41,11 @@ PATH=${MXE}/bin:${PATH}
 MXE_PKG=gdbfront-${VERSION}-win32
 ${MXEQT}/bin/qmake CONFIG+=release CONFIG+=force_debug_info gdbfront.pro
 make -j4
-pydeployqt --objdump ${MXE_TRIPLE}-objdump ${PWD}/build/gdbfront.exe \
+mkdir ${MXE_PKG}
+mv ${PWD}/release/gdbfront.exe ${MXE_PKG}
+pydeployqt --objdump ${MXE_TRIPLE}-objdump ${MXE_PKG}/gdbfront.exe \
 	--libs ${MXE}/${MXE_TRIPLE}/bin/:${MXEQT}/bin/:${MXEQT}/lib/ \
 	--extradll Qt5Svg.dll:Qt5Qml.dll:libjpeg-9.dll \
 	--qmake ${MXEQT}/bin/qmake
-mv build ${MXE_PKG}
 
 zip -9 -r ${MXE_PKG}.zip ${MXE_PKG}
